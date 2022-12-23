60899958fa8e6d6e944755458e80a8b02b8a0dda
LelisCis <leliuscris@gmail.com>
2022-12-23 02:30:15 -0300

    act

---
+++ js/appi.js
@@ -0,0 +1,15 @@
+(function() {
+    const ishow = 115;
+    const $divtop = document.getElementById("div-totop")
+    window.addEventListener("scroll", function() {
+        if(document.documentElement.scrollTop > ishow){
+            $divtop.style.display = "inherit"
+        }
+        else {
+            $divtop.style.display = "none"
+        }
+    })
+})()
+
+
+
